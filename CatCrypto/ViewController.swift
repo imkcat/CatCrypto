b486dbba99f0b39ebabad7d2aaaf826a0532de52
Kcat <kcatdeveloper@icloud.com>
2017-12-10 21:58:35 +0800

    IniInitial project

---
+++ CatCrypto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatCrypto
+//
+//  Created by Kcat on 2017/12/10.
+//  Copyright © 2017年 imkcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
