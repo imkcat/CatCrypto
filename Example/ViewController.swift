50012204121b9cef55a026cb38de5dac1706b4ff
Kcat <kcatdeveloper@icloud.com>
2017-12-11 11:36:45 +0800

    Add demo program

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,7 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let argon2Crypto = CatArgon2Crypto()
+        argon2Crypto.context.mode = .Argon2i
+        
+        var timeCount = 0.00
+        let timer = DispatchSource.makeTimerSource()
+        timer.schedule(deadline: DispatchTime.now(), repeating: 0.01)
+        timer.setEventHandler {
+            timeCount += 0.01
+        }
+        timer.resume()
+        
+        print("Let's hash 50 UUID string with argon2 in 2i mode")
+        
+        for _ in 1...50 {
+            argon2Crypto.context.salt = UUID().uuidString
+            argon2Crypto.hash(password: UUID().uuidString, completeHandler: { (hashResult) in
+                if hashResult.error == nil {
+                    print(hashResult.value!)
+                } else {
+                    print(hashResult.error!.errorDescription!)
+                }
+            })
+        }
+        timer.cancel()
+        
+        print("Hash job is done!")
+        print(String(format: "Time: %0.2fs", timeCount))
     }
 
     override func didReceiveMemoryWarning() {
