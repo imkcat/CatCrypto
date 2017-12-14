3e43d6802f880e70801082b8285dc2088bf8dc47
Kcat <kcatdeveloper@icloud.com>
2017-12-14 11:48:56 +0800

    Add mode switch button and salt textfield to demo

--- Example/Argon2ViewController.swift
+++ Example/Argon2ViewController.swift
@@ -11,17 +11,48 @@ import CatCrypto
 
 class Argon2ViewController: UIViewController {
 
+    @IBOutlet weak var saltTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var hashTextView: UITextView!
     let argon2Crypto = CatArgon2Crypto()
+    var argon2Mode: CatArgon2ContextMode = .Argon2i {
+        didSet {
+            switch argon2Mode {
+            case .Argon2d:
+                title = "Argon2d"
+            case .Argon2i:
+                title = "Argon2i"
+            case .Argon2id:
+                title = "Argon2id"
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        argon2Mode = .Argon2i
+    }
+    
+    @IBAction func modeAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Mode switch", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Argon2d", style: .default, handler: { (alertAction) in
+            self.argon2Mode = .Argon2d
+        }))
+        alertController.addAction(UIAlertAction(title: "Argon2i", style: .default, handler: { (alertAction) in
+            self.argon2Mode = .Argon2i
+        }))
+        alertController.addAction(UIAlertAction(title: "Argon2id", style: .default, handler: { (alertAction) in
+            self.argon2Mode = .Argon2id
+        }))
+        self.present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func hashAction(_ sender: Any) {
         passwordTextField.resignFirstResponder()
         
+        argon2Crypto.context.salt = saltTextField.text ?? ""
+        argon2Crypto.context.mode = argon2Mode
         argon2Crypto.hash(password: passwordTextField.text ?? "", completeHandler: { (hashResult) in
             if hashResult.error == nil {
                 self.hashTextView.text = hashResult.value!
