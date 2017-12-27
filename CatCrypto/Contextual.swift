dcfff7007812e138da98c0106dfa7a98060e5243
Kcat <kcatdeveloper@icloud.com>
2017-12-27 21:15:19 +0800

    Add Contextual protocol

---
+++ CatCrypto/Contextual.swift
@@ -0,0 +1,42 @@
+//
+//  Contextual.swift
+//  CatCrypto
+//
+//  Created by Kcat on 2017/12/27.
+//  Copyright © 2017年 imkcat. All rights reserved.
+//
+// https://github.com/ImKcat/CatCrypto
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import Foundation
+
+/// `Contextual` protocol defines the interface about context need.
+public protocol Contextual {
+    
+    /// `Context` type is the context class that for use.
+    associatedtype Context
+    
+    /// Initialize with a context object.
+    ///
+    /// - Parameter context: Context object that subclass from
+    /// `CatCryptoContext`.
+    init(context: Context)
+}
