61727e9d5247d1ec2336c1822f8292271bf33ce9
Kcat <kcatdeveloper@icloud.com>
2018-09-25 19:13:17 +0800

    Renamed AES test file

--- CatCrypto-iOSTests/AESTests.swift
+++ CatCrypto-iOSTests/AESTests.swift
@@ -1,5 +1,5 @@
 //
-//  AES.swift
+//  AESTests.swift
 //  CatCrypto-iOSTests
 //
 //  Created by Kcat on 2018/1/12.
@@ -8,34 +8,17 @@
 
 import XCTest
 @testable import CatCrypto
-import CommonCrypto
 
 class AES: XCTestCase {
 
-    var aesCrypto: CatCCEncryptionCrypto!
-
     override func setUp() {
         super.setUp()
-        aesCrypto = CatCCEncryptionCrypto()
     }
 
     override func tearDown() {
         super.tearDown()
     }
 
-    func testNormalEncrypt() {
-        aesCrypto.algorithm = .aes
-        aesCrypto.mode = .ecb
-        print([UInt8]("CatCrypt".utf8))
-        let encryptedResult = aesCrypto.encrypt(password: "CatCry")
-        let encryptedRaw = encryptedResult.raw as? [UInt8] ?? []
-        let decryptedResult = aesCrypto.commonCryptoOperation(operation: .decrypt, raw: encryptedRaw)
-        print(decryptedResult.raw)
-        print(decryptedResult.stringValue())
-//        print(encryptedResult.hexStringValue())
-//        print(aesCrypto.decrypt(encryptedPassword: encryptedResult.stringValue()!).stringValue())
-//        let decryptedString = aesCrypto.decrypt(encryptedPassword: encryptedString!).stringValue()
-//        print(decryptedString!)
-    }
+    func testAESHexEncrypt() {}
 
 }
